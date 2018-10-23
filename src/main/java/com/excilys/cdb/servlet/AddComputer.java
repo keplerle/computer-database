f5cd0e47da7faf4bdbd8a42fd7252e7b4f7f272f
keplerle <kepler.le@free.fr>
2018-10-23 15:03:16 +0200

    reglage du serveur Tomcat et modification des servlets et des jsp

---
+++ src/main/java/com/excilys/cdb/servlet/AddComputer.java
@@ -0,0 +1,14 @@
+package com.excilys.cdb.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddComputer extends HttpServlet{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/views/addComputer.jsp" ).forward( request, response );
+	}
+	
+
+}
--- src/main/java/com/excilys/cdb/servlet/Dashboard.java
+++ src/main/java/com/excilys/cdb/servlet/Dashboard.java
@@ -1,16 +1,14 @@
 package com.excilys.cdb.servlet;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-public class CompanyServlet extends HttpServlet{
 
+public class Dashboard extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String message = "Transmission de variables : OK !";
-		request.setAttribute( "test", message );
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/views/test.jsp" ).forward( request, response );
+		this.getServletContext().getRequestDispatcher("/WEB-INF/views/dashboard.jsp").forward(request, response);
 	}
 }
