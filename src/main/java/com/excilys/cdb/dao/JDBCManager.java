8d0af2a7590b0c5640e0fbc6e4db159bfe521ee2
keplerle <kepler.le@free.fr>
2018-11-03 12:08:55 +0100

    hikariCP: correction

--- src/main/java/com/excilys/cdb/dao/JDBCManager.java
+++ src/main/java/com/excilys/cdb/dao/JDBCManager.java
@@ -1,12 +1,9 @@
 package com.excilys.cdb.dao;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
